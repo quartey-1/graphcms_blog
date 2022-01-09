a8c6b9b510a9c60ea5f85b1e025f89066bfabeb4
Alfred Quartey <qalfred8@gmail.com>
2022-01-09 21:05:21 +0100

    first commit

---
+++ components/Layout.jsx
@@ -0,0 +1,15 @@
+
+import Link from 'next/link'
+import React from 'react'
+import { Header } from './'
+
+const Layout = ({ children }) => {
+    return (
+       <div>
+        <Header />
+        { children }
+        </div>
+    )
+}
+
+export default Layout
